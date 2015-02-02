2d11b89a777995a91e645812d5b5010a652d119e
tersfeld <thomas.ersfeld@gmail.com>
2015-02-02 12:03:51 +0100

    Fixing name variable in statistics

--- bioinfo_acqui/src/Main.java
+++ bioinfo_acqui/src/Main.java
@@ -54,34 +54,5 @@ public class Main {
     }
 }
 
-class MainForm extends JFrame {
-    private JTextArea textArea = new JTextArea();
-    private JProgressBar bar = new JProgressBar();
-
-    public MainForm() {
-        super("Trinucleotide statistical analysis");
-
-        bar.setMaximum(500);
-        bar.setMinimum(0);
-        bar.setStringPainted(true);
-
-        System.setOut(new PrintStream(new TextAreaOutputStream(textArea)));
-
-        DefaultCaret caret = (DefaultCaret)textArea.getCaret();
-        caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
-
-
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.getContentPane().add(new JScrollPane(textArea), BorderLayout.CENTER);
-        this.getContentPane().add(bar, BorderLayout.NORTH);
-        this.setVisible(true);
-
-        setSize(640,480);
-
-
-
-        setVisible(true);
-    }
-}
 
 
