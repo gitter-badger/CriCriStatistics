fb813bf30130fe54b10490d4361fc908d5f51f26
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2015-02-02 10:41:33 +0100

    TODO Statistics constructor with CDS array

--- bioinfo_acqui/src/Statistics.java
+++ bioinfo_acqui/src/Statistics.java
@@ -11,50 +11,52 @@ public class Statistics
     public ArrayList<HashMap<String, Integer>> phases;
     private int shift_window_size;
 
+    // TODO: we could also add a constructor with an array of CDS (String[] seq)
+    // then the frequencies would be summed in the same HashMap (keeping phases separated of course)
     public Statistics(Alphabet alphabet, String seq) {
-    
+
         shift_window_size = alphabet.item_length;
         phases = new ArrayList<HashMap<String, Integer>>(shift_window_size);
-        
+
         // initialize list of hash to 0
         for (int i=0; i<shift_window_size; i++) {
-        
+
             HashMap<String, Integer> frequencies = new HashMap<String, Integer>();
             for (String key : alphabet.items) {
-            
+
                 frequencies.put(key, 0);
             }
             phases.add(frequencies);
         }
-        
+
         ComputeFrequencies(seq);
     }
-    
+
     private void ComputeFrequencies(String seq) {
-    
+
         for (int i=0; i<shift_window_size; i++) {
-        
+
             PhaseFrequencies(seq, i);
         }
     }
 
     private void PhaseFrequencies(String sequence, int phase) {
-    
+
         // ensure phase do not go out of bounds
         int true_phase = phase % shift_window_size;
         String work_sequence = sequence.substring(true_phase);
-        
+
         // ensure the passed String has a size multiple of shift_window_size
         int shift = work_sequence.length() % shift_window_size;
         if (shift > 0) {
-        
+
             work_sequence = work_sequence.substring(
                 0, work_sequence.length() - shift);
         }
-        
+
         // increment each met key
         for (String n_nucleotide : Split(work_sequence)) {
-        
+
             phases.get(true_phase)
                   .put(trinucleotide,
                        phases.get(true_phase)
@@ -63,14 +65,14 @@ public class Statistics
     }
 
     private List<String> Split(String s) {
-    
+
         List<String> parts = new ArrayList<String>();
         int len = s.length();
         for (int i=0; i<len; i+=shift_window_size) {
-        
+
             parts.add(s.substring(i, i+shift_window_size));
         }
-        
+
         return parts;
     }
 }
