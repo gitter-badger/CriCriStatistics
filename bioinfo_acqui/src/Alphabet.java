1067bcc3e6c7758da28652a36088d712b6332fa8
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2015-03-04 17:38:49 +0100

    Blank lines

--- bioinfo_acqui/src/Alphabet.java
+++ bioinfo_acqui/src/Alphabet.java
@@ -24,8 +24,10 @@ public class Alphabet {
     private String RemoveDuplicates(String s) {
         StringBuilder noDupes = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
+            
             String si = s.substring(i, i + 1);
             if (noDupes.indexOf(si) == -1) {
+                
                 noDupes.append(si);
             }
         }
