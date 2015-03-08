1136cd84fc296c94eb3cd9ef131eefbaf746737a
pallamidessi <joseph.pallamidessi@gmail.com>
2015-03-08 17:39:08 +0100

    Add a global setting object to easily pass parameter

---
+++ bioinfo_acqui/src/Settings.java
@@ -0,0 +1,26 @@
+import org.apache.log4j.Logger;
+
+public class Settings{
+  private int numThreads;
+  // Singleton Stuff
+  private static class SingletonHolder { 
+	     public static final Settings INSTANCE = new Settings();
+	}
+	
+  public static Settings getInstance() {
+	     return SingletonHolder.INSTANCE;
+	}
+  // End Singleton Stuff
+
+  private Settings(){
+    this.numThreads = 1;
+  }
+  
+  public void setNumThreads(int numThreads){
+    this.numThreads = numThreads;
+  }
+  
+  public int getNumThreads(){
+    return this.numThreads;
+  }
+}
