3da587e237ba31f95edfef3cd0198e7ec10f7ec5
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2015-05-04 18:40:48 +0200

    Added readGrouped stats method

--- src/ExcelReader.java
+++ src/ExcelReader.java
@@ -65,13 +65,45 @@ public class ExcelReader {
                 stats.addCount(0, key, Integer.parseInt(sheet.getCell(1, i).getContents()));
                 stats.addCount(1, key, Integer.parseInt(sheet.getCell(3, i).getContents()));
                 stats.addCount(2, key, Integer.parseInt(sheet.getCell(5, i).getContents()));
-                // Read phases key frequencies
-                stats.addFreq(0, key, (float) ((NumberCell) sheet.getCell(2, i)).getValue());
-                stats.addFreq(1, key, (float) ((NumberCell) sheet.getCell(4, i)).getValue());
-                stats.addFreq(2, key, (float) ((NumberCell) sheet.getCell(6, i)).getValue());
             }
 
             return stats;
+
+        } catch (BiffException e) {
+            System.out.println(this.inputFile);
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public WrittenStats readGrouped() throws IOException {
+        try {
+            WrittenStats stats = new WrittenStats();
+            Workbook w = Workbook.getWorkbook(this.inputFile);
+            // Get the first sheet
+            Sheet sheet = w.getSheet(0);
+
+            stats.kingdom = sheet.getCell(1, 1).getContents();
+            stats.group = sheet.getCell(2, 1).getContents();
+            stats.subgroup = sheet.getCell(3, 1).getContents();
+            stats.organism = sheet.getCell(4, 1).getContents();
+
+            // If this is a grouped stats excel file, we return null
+            if (stats.group.isEmpty() || stats.subgroup.isEmpty() || stats.organism.isEmpty()) {
+
+                for (int i=7; i<=70; i++) {
+                    String key = sheet.getCell(0, i).getContents();
+                    // Read phases key frequencies
+                    stats.addFreq(0, key, (float) ((NumberCell) sheet.getCell(2, i)).getValue());
+                    stats.addFreq(1, key, (float) ((NumberCell) sheet.getCell(4, i)).getValue());
+                    stats.addFreq(2, key, (float) ((NumberCell) sheet.getCell(6, i)).getValue());
+                }
+
+                return stats;
+            }
+            else
+                return null;
 
         } catch (BiffException e) {
             System.out.println(this.inputFile);
